ee8e9ad6f70ef3bf411e4fe6c742096bb9c6f8da
Sudhanshu <sudhanshus215@gmail.com>
2025-11-17 12:24:51 +0530

    Status Done

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group1" "rg" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
-  address_space       = ["10.0.0.2/32"]
+  address_space       = ["10.0.0/32"]
   location           = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
